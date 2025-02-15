57dddf110875704469204a1624573ecd17fbf00a
Yuvi <yuvraj@indieverse.studio>
2025-02-15 20:15:49 +0530

    Implement CanvasVisibilityHandler and DrawingManager with visibility toggling and drawing functionality

--- CanvasVisibilityHandler.cs
+++ CanvasVisibilityHandler.cs
@@ -1,6 +1,40 @@
-﻿namespace LiveDraw;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LiveDraw;
 
 public class CanvasVisibilityHandler
 {
-    
+    private enum CanvasVisibilityMode
+    {
+        TRANSPARENT,
+        INVISIBLE,
+        VISIBILE,
+    }
+    private CanvasVisibilityMode _canvasVisibilityMode = CanvasVisibilityMode.TRANSPARENT;
+
+    public void ToggleVisibility(Window window, Canvas canvas)
+    {
+        switch (_canvasVisibilityMode)
+        {
+            case CanvasVisibilityMode.TRANSPARENT:
+                canvas.Background = Brushes.Transparent;
+                window.Background = (Brush)new BrushConverter().ConvertFromString("#00000000");
+                _canvasVisibilityMode = CanvasVisibilityMode.INVISIBLE;
+                break;
+            case CanvasVisibilityMode.INVISIBLE:
+                canvas.Background = Brushes.White;
+                window.Background = (Brush)new BrushConverter().ConvertFromString("#01000000");
+                _canvasVisibilityMode = CanvasVisibilityMode.VISIBILE;
+                break;
+            case CanvasVisibilityMode.VISIBILE:
+                canvas.Background = Brushes.Transparent;
+                window.Background = (Brush)new BrushConverter().ConvertFromString("#01000000");
+                _canvasVisibilityMode = CanvasVisibilityMode.TRANSPARENT;
+                break;
+            default:
+                break;
+        }
+    }
 }
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace LiveDraw;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private double _width = 0;
+    private double _height = 0;
+
+    private readonly DrawingManager _drawingManager = new();
+    private readonly CanvasVisibilityHandler _canvasVisibilityHandler = new();
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object obj, RoutedEventArgs e)
+    {
+        this.Topmost = true;
+        _width = this.ActualWidth;
+        _height = this.ActualHeight;
+            
+        Canvas.SetLeft(ToolBar, (_width / 2) - (ToolBar.ActualHeight/2));
+        Canvas.SetTop(ToolBar, 50);
+        
+        _drawingManager.Initialize();
+    }
+
+    private void Canvas_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            _drawingManager.StartDrawing(MainCanvas);
+        }
+    }
+
+    private void Canvas_OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+        _drawingManager.StopDrawing();
+    }
+
+    private void Canvas_OnMouseMove(object sender, MouseEventArgs e)
+    {
+        _drawingManager.ContinueDrawing(e.GetPosition(MainCanvas));
+    }
+    private void ToggleCanvasVisibility_OnClick(object sender, RoutedEventArgs e)
+    {
+        _canvasVisibilityHandler.ToggleVisibility(this, MainCanvas);
+    }
+}
